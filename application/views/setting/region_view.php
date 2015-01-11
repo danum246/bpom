9d322f45f30fdb3718c6f8f578b4943d93f18928
danum246 <danum246@gmail.com>
2015-01-11 22:08:59 +0700

    region

---
+++ application/views/setting/region_view.php
@@ -0,0 +1,133 @@
+<div class="row">
+  	<div class="span12">      		
+  		<div class="widget ">
+  			<div class="widget-header">
+  				<i class="icon-user"></i>
+  				<h3>Data Region</h3>
+			</div> <!-- /widget-header -->
+			<div class="widget-content">
+				<div class="tabbable">
+					<ul class="nav nav-tabs">
+					  <li class="active"><a href="#provinsi" data-toggle="tab">Provinsi</a></li>
+					  <li><a href="#kota" data-toggle="tab">Kabupaten / Kota</a></li>
+					  <li><a href="#camat" data-toggle="tab">Kecamatan</a></li>
+					  <li><a href="#lurah" data-toggle="tab">Kelurahan</a></li>
+					</ul>
+					<div class="tab-content">
+						<div class="tab-pane active" id="provinsi">
+							<div class="span11">
+								<a data-toggle="modal" href="#myProv" class="btn btn-primary"> Data Baru </a><br><hr>
+								<table id="example1" class="table table-bordered table-striped">
+				                	<thead>
+				                        <tr> 
+				                        	<th>No</th>
+				                        	<th>Provinsi</th>
+			                                <th width="80"></th>
+				                        </tr>
+				                    </thead>
+				                    <tbody>
+				                        <tr>
+				                        	<td>1</td>
+				                        	<td>DKI Jakarta</td>
+				                        	<td class="td-actions">
+												<a class="btn btn-primary btn-small" href="#"><i class="btn-icon-only icon-pencil"> </i></a>
+												<a class="btn btn-danger btn-small" href="#"><i class="btn-icon-only icon-remove"> </i></a>
+											</td>
+				                        </tr>
+				                      
+				                    </tbody>
+				               	</table>
+							</div>
+						</div>
+						<div class="tab-pane" id="kota">
+							<div class="span11">
+								<a data-toggle="modal" href="#myKota" class="btn btn-primary"> Data Baru </a><br><hr>
+								<table id="example3" class="table table-bordered table-striped">
+				                	<thead>
+				                        <tr> 
+				                        	<th>No</th>
+				                        	<th>Kabupaten / Kota</th>
+				                        	<th>Provinsi</th>
+			                                <th width="80"></th>
+				                        </tr>
+				                    </thead>
+				                    <tbody>
+				                        <tr>
+				                        	<td>1</td>
+				                        	<td>Jakarta Timur</td>
+				                        	<td>DKI Jakarta</td>
+				                        	<td class="td-actions">
+												<a class="btn btn-primary btn-small" href="#"><i class="btn-icon-only icon-pencil"> </i></a>
+												<a class="btn btn-danger btn-small" href="#"><i class="btn-icon-only icon-remove"> </i></a>
+											</td>
+				                        </tr>
+				                    </tbody>
+				               	</table>
+							</div>
+						</div>
+						<div class="tab-pane" id="camat">
+							<div class="span11">
+								<a data-toggle="modal" href="#myCamat" class="btn btn-primary"> Data Baru </a><br><hr>
+								<table id="example4" class="table table-bordered table-striped">
+				                	<thead>
+				                        <tr> 
+				                        	<th>No</th>
+				                        	<th>Kecamatan</th>
+				                        	<th>Kabupaten/Kota</th>
+				                        	<th>Provinsi</th>
+			                                <th width="80"></th>
+				                        </tr>
+				                    </thead>
+				                    <tbody>
+				                        <tr>
+				                        	<td>1</td>
+				                        	<td>Matraman</td>
+				                        	<td>Jakarta Timur</td>
+				                        	<td>DKI Jakarta</td>
+				                        	<td class="td-actions">
+												<a class="btn btn-primary btn-small" href="#"><i class="btn-icon-only icon-pencil"> </i></a>
+												<a class="btn btn-danger btn-small" href="#"><i class="btn-icon-only icon-remove"> </i></a>
+											</td>
+				                        </tr>
+				                      
+				                    </tbody>
+				               	</table>
+							</div>
+						</div>
+						<div class="tab-pane" id="lurah">
+							<div class="span11">
+								<a data-toggle="modal" href="#myLurah" class="btn btn-primary"> Data Baru </a><br><hr>
+								<table id="example5" class="table table-bordered table-striped">
+				                	<thead>
+				                        <tr> 
+				                        	<th>No</th>
+				                        	<th>Kelurahan</th>
+				                        	<th>Kecamatan</th>
+				                        	<th>Kabupaten/Kota</th>
+				                        	<th>Provinsi</th>
+			                                <th width="80"></th>
+				                        </tr>
+				                    </thead>
+				                    <tbody>
+				                        <tr>
+				                        	<td>1</td>
+				                        	<td>Palmeriam</td>
+				                        	<td>Matraman</td>
+				                        	<td>Jakarta Timur</td>
+				                        	<td>DKI Jakarta</td>
+				                        	<td class="td-actions">
+												<a class="btn btn-primary btn-small" href="#"><i class="btn-icon-only icon-pencil"> </i></a>
+												<a class="btn btn-danger btn-small" href="#"><i class="btn-icon-only icon-remove"> </i></a>
+											</td>
+				                        </tr>
+				                      
+				                    </tbody>
+				               	</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
