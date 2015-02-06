5a34d39f1605e817bb8226e6aa139b2d68efc0c9
Danu Mahendra <danum246@gmail.com>
2015-02-06 23:22:04 +0700

    penyampaian

---
+++ application/controllers/form/penyampaian.php
@@ -0,0 +1,28 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class penyampaian extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		if ($this->session->userdata('sess_login') != TRUE) {
+		 	redirect('auth','refresh');
+		}
+	}
+
+	function index()
+	{	
+		$data['page'] = 'form/penyampaian_view';
+		$this->load->view('template',$data);
+	}
+
+	function detail()
+	{
+		$data['page'] = 'form/detail_penyampaian_view';
+		$this->load->view('template',$data);
+	}
+
+}
+
+/* End of file penyampaian.php */
+/* Location: ./application/controllers/form/penyampaian.php */
